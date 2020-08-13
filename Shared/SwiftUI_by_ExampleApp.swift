82681312c72025adfe913fa2a5ace647335dd54e
bmaciag <barm@live.de>
2020-08-13 21:32:39 +0200

    Initial Commit

---
+++ Shared/SwiftUI_by_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_by_ExampleApp.swift
+//  Shared
+//
+//  Created by Bartholomäus Maciag on 13.08.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_by_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
